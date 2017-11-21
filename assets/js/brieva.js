fed718d780edc5ce15588956249c02d628110375
Caballero73 <comisionado.falcon@gmail.com>
2017-11-21 11:09:20 -0400

    Completado efecto del menú que convierte el Burger-button en una X

---
+++ assets/js/brieva.js
@@ -0,0 +1,21 @@
+$('#burger-button').click(function() {
+	$('#menu-bars').toggleClass('active');
+	$('body').toggleClass('fijo');
+	$('#menu').toggleClass('active')
+	setTimeout(function() {
+		$('#burger-button').toggleClass('burger-color')
+	}, 300)
+})
+
+$('#burger-button').click(function() {
+	$('#bars').toggleClass('x');
+	$('#bars').toggleClass('x');
+	$('#bars').toggleClass('x');
+})	
+
+$('.link-menu').click(function() {
+	$('#menu').toggleClass('active');
+	$('body').toggleClass('fijo');
+	$('#burger-button').toggleClass('burger-color');
+	$('#menu-bars').toggleClass('active')
+})
